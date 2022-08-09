d6e7a3671a0180ec3aa23dd2c1b81c525b0c3641
Shred1 <shredder842@gmail.com>
2022-08-09 22:25:48 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+namespace assignment_INEC
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] voters = { "Mary", "Sean", "James", "Mary", "Jean", "Jean", "Mary", "Olu", "Seun", "Bose", "James", "Mary" };
+           
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+            foreach (var item in voters)
+            {
+                if (dic.ContainsKey(item))
+                {
+                    dic[item]++;
+                }
+                else
+                {
+                    dic[item] = 1;
+                }
+
+            }
+            Console.WriteLine("Name and number of times registered");
+            foreach (var item in dic)
+            {
+                if (item.Value>1)
+                {
+                    Console.WriteLine(item.Key + ": " + item.Value);
+                }
+            }
+            Console.ReadLine();
+
+
+            //INEC should use HashSet next time to avoid duplicates.
+
+
+
+
+       
+
+        }
+    }
+}
